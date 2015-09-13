9e5684490a8680401b6beb5571d0d7a9ea4728d6
nishitsardessai <nishit121@gmail.com>
2015-09-13 19:05:09 +0530

    new resolutions

--- resolutions.js
+++ resolutions.js
@@ -1,17 +1,26 @@
+Resolutions = new Mongo.Collection('resolutions')
+
 if (Meteor.isClient) {
   Template.body.helpers({
-    resolutions:[
-    { title: "hellodjsiknfsjin"},
-    { title: "vsfijbikjds"},
-    { title: "jcbsdjkcjsdnsjidk"}]
+    resolutions: function(){
+      return Resolutions.find();
+    }
   });
 
+  Template.body.events({
+    'submit.new-resolution': function(event){
+      var title= event.target.title.value;
 
+      Resolutions.insert({
+        title:title,
+        createdAt: new Date()
+      });
 
+      event.target.title.value="";
+      return false;
+    }
 
-
-
-
+  });
 }
 
 if (Meteor.isServer) {
